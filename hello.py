42993ad4571b2ef438312e7d86a02a7c5461e5c1
jzsun <sunjz1989@gmail.com>
2015-11-02 23:10:59 +0800

    python: base

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#-*-coding:utf-8-*-
 
 #I learn python in  2014, but I don't study hard .
 #Indeed ,I am still a beginner.
@@ -7,5 +8,25 @@
 #print "2015-11-01"
 #print "Hello, Python again !"
 
-name = raw_input("Please input your name!")
-print "Hello",name
+#name = raw_input("Please input your name!")
+#print "Hello",name
+
+#print ord("A") #65
+#print chr(65) #A
+
+#ERROR
+#must add #-*-coding:utf-8-*-
+#print u'你好!'
+
+#格式化
+#print "Hello, %s" % "world"
+#print 'Hi, %s, you have $%d.' % ("sundashe", 100000000) 
+
+#补零，和小数的位数
+print "%2d-%02d" % (3, 1)
+print '%.2f' % 3.1415926
+
+#不确定用什么时，就用%s
+
+#%作为普通字符需要转义
+print 'growth rate:%d %%' % 7
