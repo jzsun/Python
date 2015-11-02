b57350a473997983fc6e43a87a6498192b08cfdc
jzsun <sunjz1989@gmail.com>
2015-11-02 22:25:21 +0800

    python: raw_iput("")

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
-__author__ = 'root'
+#!/usr/bin/env python
 
 #I learn python in  2014, but I don't study hard .
 #Indeed ,I am still a beginner.
 # I must learn it again.
-print "2015-11-01"
-print "Hello, Python again !"
 
-pow(2, 3)
+#print "2015-11-01"
+#print "Hello, Python again !"
+
+name = raw_input("Please input your name!")
+print "Hello",name
